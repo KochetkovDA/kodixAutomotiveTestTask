15c3377de1fda9ecf2d2e0b8d4f9d44e9a37a91a
Danil Kochetkov <kochetkov214@gmail.com>
2020-08-16 23:35:44 +0700

    Добавляет базовую страницу для отображения в router view, добавляет на page хэдер и футер, отображает данные из стора для примера

---
+++ src/views/page.vue
@@ -0,0 +1,42 @@
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+import headerBar from '@/components/header';
+import footerBar from '@/components/footer';
+export default {
+  name: 'Page',
+  components: { headerBar, footerBar },
+  computed: {
+    ...mapGetters('cars', ['carsList']),
+  },
+  created() {
+    this.fetchCars();
+  },
+
+  methods: {
+    ...mapActions('cars', ['fetchCars']),
+  },
+};
+</script>
+
+<template>
+  <div>
+    <header-bar></header-bar>
+    <div class="content-wrapper">
+      <ul>
+        <li v-for="(car, index) in carsList" :key="`${car.id}_${index}`">
+          {{ car }}
+        </li>
+      </ul>
+    </div>
+    <footerBar></footerBar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.content-wrapper {
+  padding-top: 104px;
+  width: 100%;
+  overflow: auto;
+}
+</style>
